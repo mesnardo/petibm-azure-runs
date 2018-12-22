fdb6bc264c6cd57f5f7876b7c38d57ddb02050d4
mesnardo <mesnardo@gwu.edu>
2018-12-21 19:12:38 -0500

    Simplify script to upload data on error

--- misc/copy-on-error-template.sh
+++ misc/copy-on-error-template.sh
@@ -2,12 +2,10 @@
 
 rc=$?
 if [ $rc != 0 ]; then
-	storageaccountname="<storage-account-name>"
-	sharename="<storage-share-name>"
-	mountpoint="/mnt/$sharename"
-	mkdir -p $mountpoint
-	storageaccountkey="<storage-account-key>"
-	mount -t cifs //$storageaccountname.file.core.windows.net/$sharename $mountpoint -o vers=3.0,username=$storageaccountname,password=$storageaccountkey,dir_mode=0777,file_mode=0777,sec=ntlmssp
-	cp -r $simudir $mountpoint
+	mountpoint="$AZ_BATCH_NODE_ROOT_DIR/mounts"
+	accountname="<storage-account-name>"
+	fileshare="<storage-share-name>"
+	rm -rf core.*
+	cp -r $simudir $mountpoint/azfile-$accountname-$fileshare
 fi
 exit $rc
